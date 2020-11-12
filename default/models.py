94c24d9cbb8bcb693c9ef73847a447bc21d494e1
tseng yi <10935013@dcsh.tp.edu.tw>
2020-11-12 10:36:07 +0800

    投票主題列表.檢視.投票

--- default/models.py
+++ default/models.py
@@ -8,12 +8,13 @@ class Poll(models.Model):
 
     def __str__(self):
         return str(self.id)+")"+self.suject
-    #opt1 = models.CharField(max_length=225)
-    #opt2 = models.CharField(max_length=225)
-    #opt3 = models.CharField(max_length=225)
-    #opt4 = models.CharField(max_length=225)
-
+        
 class Option(models.Model):
     poll_id = models.IntegerField()
     title = models.CharField(max_length=225)
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id) + ")"+ self.title + "@"+str(self.poll_id)
+
+    
